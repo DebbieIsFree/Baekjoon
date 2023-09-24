7373e1cc78aec71feb7bb873348b0a889ecd404c
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2023-09-24 19:07:26 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 111.17 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    map<string, int> m;
+    
+    for(int i=0; i<participant.size(); i++){
+        string name = participant[i];
+        m[name] += 1;
+    }
+    
+    for(int i=0; i<completion.size(); i++){
+        string name = completion[i];
+        m[name] -= 1;
+    }
+    
+    for(auto each : m){
+        if(each.second == 1){
+            answer = each.first;
+        }
+    }
+    
+    return answer;
+}
