dba96fc2e8bdfdaca127e800e3262f95266a3685
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2023-09-24 20:12:02 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    
+    num % 2 == 0 ? answer = "Even" : answer = "Odd";
+    
+    return answer;
+}
