f20e9e84eb063425f92b4b3c8571de96e99bbcb5
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2023-09-25 20:29:02 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.38 ms, Memory: 25.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -1,28 +1,20 @@
 #include <string>
 #include <vector>
 #include <map>
+#include <algorithm>
 using namespace std;
 
 string solution(vector<string> participant, vector<string> completion) {
-    string answer = "";
-    map<string, int> m;
+     sort(participant.begin(), participant.end());
+    sort(completion.begin(), completion.end());
     
-    for(int i=0; i<participant.size(); i++){
-        string name = participant[i];
-        m[name] += 1;
-    }
-    
-    for(int i=0; i<completion.size(); i++){
-        string name = completion[i];
-        m[name] -= 1;
-    }
-    
-    for(auto each : m){
-        if(each.second == 1){
-            answer = each.first;
+    int i = 0;
+    for(; i<completion.size(); i++){
+        if(completion[i] != participant[i]){
             break;
         }
     }
     
-    return answer;
+
+    return participant[i];
 }
