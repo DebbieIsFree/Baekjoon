a3a57de3612a7322e8a66c3888242142944385ed
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2023-10-28 15:52:08 +0900

    [level 1] Title: 옹알이 (2), Time: 0.03 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<string> b) {
+    int answer = 0;
+    
+    for(int i=0; i<b.size(); i++){
+        string tmp = "";
+        string prev = "";
+        int cnt = 0;
+        
+        for(int j=0; j<b[i].length(); j++){
+            tmp += b[i][j];
+         
+            if(tmp == "aya" || tmp =="ma" || tmp =="woo" || tmp=="ye"){
+                if(prev != tmp){
+                    cnt++;
+                    prev = tmp;
+                    tmp = "";    
+                }
+                else {
+                    break;
+                }        
+            }
+        }   
+        if(tmp != "")
+            cnt = 0;
+        if(cnt > 0)
+            answer++;
+    }
+
+    
+    
+    return answer;
+}
+
+
