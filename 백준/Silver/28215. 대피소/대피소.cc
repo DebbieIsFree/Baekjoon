011249aa68740a82b9097f3294cf73f253e82a72
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2023-11-28 12:22:32 +0900

    Update 대피소.cc

--- 백준/Silver/28215. 대피소/대피소.cc
+++ 백준/Silver/28215. 대피소/대피소.cc
@@ -1,43 +1,49 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+#include <math.h>
 #include <algorithm>
 using namespace std;
 
-int main() {
+int main(){
+    
     int N, K;
     cin >> N >> K;
-
+    
     vector<pair<int, int>> v;
-
+    
     int mx = 0, my = 0;
-
-    for (int i = 0; i < N; i++) {
-        int x, y;
-        cin >> x >> y;
+    
+    for(int i=0; i<N; i++){
+        int x, y;   cin >> x >> y;
         v.push_back(make_pair(x, y));
-        mx = max(mx, x);
-        my = max(my, y);
+        if(mx < x)
+            mx = x;
+        if(my < y)
+            my = y;
     }
-
+    
     vector<int> ans;
-
-    for (int i = 0; i <= my; i++) {
-        for (int j = 0; j <= mx; j++) {
+    
+    for(int i=0; i<=my; i++){
+        for(int j=0; j<=mx; j++){
             int m = 0;
-            for (int k = 0; k < N; k++) {
+            for(int k=0; k<N; k++){
                 int tmp = abs(v[k].first - j) + abs(v[k].second - i);
-                if (m < tmp) {
+                if(m < tmp){
                     m = tmp;
                 }
             }
             ans.push_back(m);
         }
     }
-
+    
     sort(ans.begin(), ans.end());
-
+    
     cout << ans[0];
 
     return 0;
 }
+
+
+
+
