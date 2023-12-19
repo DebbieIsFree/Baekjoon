d7dba2ae326c35c2e7100541e710592dddd2b325
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2023-12-19 18:34:43 +0900

    [Silver V] Title: 스네이크버드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main(){
+    
+    int N, L;
+    cin >> N >> L;
+    
+    vector<int> v(N);
+    
+    for(int i=0; i<N; i++){
+        cin >> v[i];
+    }
+    
+    sort(v.begin(), v.end());
+
+    for(int i=0; i<N; i++){
+        if(v[i] <= L){
+            L++;
+        }
+    }
+
+    cout << L;
+    
+    return 0;
+}
