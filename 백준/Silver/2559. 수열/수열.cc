0d6f917a6f4dd24aa37faa34c738a17bc2c46b07
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2023-12-21 05:35:11 +0900

    Update 수열.cc

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -3,29 +3,30 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
+int main(){
     
     int N, K;
     cin >> N >> K;
     
-    vector<int> v(N + 1, 0);
-    vector<int> arr(N + 1, 0);
+    vector<int> v(N+1, 0);
+    vector<int> arr(N+1, 0);
     
-    for (int i = 1; i <= N; i++) {
+    for(int i=1; i<=N; i++){
         cin >> v[i];
     }
     
-    for (int i = 1; i <= N; i++) {
-        arr[i] = arr[i - 1] + v[i];
+    for(int i=1; i<=N; i++){
+        arr[i] = arr[i-1] + v[i];
     }
 
-    int res = arr[K];
+    // 각각의 수는 -100 이상 100 이하의 값을 가지므로, N이 최대 10^5일 때 모든 값이 -100보다 큰
+    // 숫자로 초기화 한다.
+    int res = -987654321;
     
-    for (int i = K; i <= N; i++) {
-        int current_sum = arr[i] - arr[i - K];
-        res = max(res, current_sum);
+    for (int i=K; i<=N; i++) {
+        int tmp = arr[i] - arr[i - K];
+        res = max(res, tmp);
     }
-    
     cout << res;
 
     return 0;
