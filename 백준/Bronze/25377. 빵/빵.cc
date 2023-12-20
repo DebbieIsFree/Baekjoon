c72a435090c53c5913519605d92784e327eaf18a
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2023-12-20 21:20:11 +0900

    [Bronze IV] Title: 빵, Time: 0 ms, Memory: 2024 KB, Score: 33 point -BaekjoonHub

---
+++ 백준/Bronze/25377. 빵/빵.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    
+    int N;  cin >> N;
+    
+    vector<pair<int, int>> v;
+    
+    while(N--){
+        int go, bread;
+        cin >> go >> bread;
+        v.push_back({go, bread});
+    }
+    
+    sort(v.begin(), v.end(), greater<pair<int, int>>());
+    
+    int res = 99999;
+    
+    for(int i=0; i<v.size(); i++){
+        if(v[i].second >= v[i].first){
+            if(res > v[i].second){
+                res = v[i].second;
+            }
+        }
+    }
+    
+    if(res != 9999)
+        cout << res;
+    else 
+        cout << "-1";
+    
+    return 0;
+}
