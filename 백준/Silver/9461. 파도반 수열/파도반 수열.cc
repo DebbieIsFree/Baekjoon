1d03aa7a3ac0776fcefb559adf131acc489e62e6
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2023-12-22 15:46:39 +0900

    [Silver III] Title: 파도반 수열, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+long long dp[101];
+
+int main(){
+    
+    int T;  cin >> T;
+    
+    dp[1] = 1;
+    dp[2] = 1;
+    dp[3] = 1;
+    
+    for(int i=4; i<=100; i++){
+        dp[i] = dp[i-3] + dp[i-2];
+    }
+    
+    
+    while(T--){
+        int n;  cin >> n;
+        
+        cout << dp[n] << "\n";
+    }
+
+    return 0;
+}
