28a782c41f4970f9f07a9a4b147a1ea05657b13e
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-03-02 21:06:31 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// 그리디 알고리즘 (구현)
+
+int P[1001];
+
+int main() {
+	int N;	cin >> N;
+	for (int i = 0; i < N; i++)
+		cin >> P[i];
+
+	sort(P, P + N);
+
+	int result = 0;
+	for (int i = 0; i < N; i++) {
+		int sum = 0;
+		for (int j = 0; j <= i; j++) {
+			sum += P[j];
+		}
+		result += sum;
+	}
+
+	cout << result;
+	
+	return 0;
+}
