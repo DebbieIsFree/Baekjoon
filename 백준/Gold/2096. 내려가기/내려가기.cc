dea4934c5f2fbe821738e7e1c47bc70de92079fc
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-04-12 17:17:59 +0900

    [Gold V] Title: 내려가기, Time: 56 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.cc
+++ 백준/Gold/2096. 내려가기/내려가기.cc
@@ -2,52 +2,27 @@
 #include <algorithm>
 using namespace std;
 
-// 메모리 초과
-// int arr[100001][3]       // 입력값
-// int max_dp[100001][3]    // 최대값 dp
-// int min_dp[100001][3]    // 최소값 dp 
-
-// --> 슬라이딩 윈도우
-
 int min_dp[3];
 int max_dp[3];
 
 int main(){
-    int N;  cin >> N;
-    
-    // 입력 받으면서 바로 계산
+    int N, input1, input2, input3, tmp1, tmp2;  cin >> N;
     
     for(int i=1; i<=N; i++){
-        int tmp0 = max_dp[0];
-        int tmp1 = max_dp[1];
-        int tmp2 = max_dp[2];
-        
-        int t0 = min_dp[0];
-        int t1 = min_dp[1];
-        int t2 = min_dp[2];
-        
-        for(int j=0; j<3; j++){
-            int input;
-            cin >> input;
-
-            // max_dp
-            if(j == 0)
-                max_dp[0] = max(tmp0, tmp1) + input;
-            else if(j == 1)
-                max_dp[1] = max(tmp0, max(tmp1, tmp2)) + input;
-            else if(j == 2)
-                max_dp[2] = max(tmp1, tmp2) + input;
+            cin >> input1 >> input2 >> input3;
+            tmp1 = max_dp[0];
+            tmp2 = max_dp[2];
+            max_dp[0] = max(max_dp[0], max_dp[1]) + input1;
+            max_dp[2] = max(max_dp[1], max_dp[2]) + input3;
+            max_dp[1] = max(max_dp[1], max(tmp1, tmp2)) + input2;
             
-            
-            // min_dp
-            if(j== 0)
-                min_dp[0] = min(t0, t1) + input;
-            else if(j == 1)
-                min_dp[1] = min(t0, min(t1, t2)) + input;
-            else if(j == 2)
-                min_dp[2] = min(t1, t2) + input;
-        }
+            tmp1 = min_dp[0];
+            tmp2 = min_dp[2];
+            min_dp[0] = min(min_dp[0], min_dp[1]) + input1;
+            min_dp[2] = min(min_dp[1], min_dp[2]) + input3;
+            min_dp[1] = min(min_dp[1], min(tmp1, tmp2)) + input2;
     }
+    
     
     int min_ans = min(min_dp[0], min(min_dp[1], min_dp[2]));
     int max_ans = max(max_dp[0], max(max_dp[1], max_dp[2]));
@@ -56,4 +31,3 @@ int main(){
 
     return 0;
 }
-
