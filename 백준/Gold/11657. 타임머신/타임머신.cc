71a67d6a0d37d3bb4019debd891a6552198b3bf4
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-04-19 00:29:51 +0900

    [Gold IV] Title: 타임머신, Time: 16 ms, Memory: 2220 KB -BaekjoonHub

---
+++ 백준/Gold/11657. 타임머신/타임머신.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+struct edge{
+  int src, dest, cost;  
+};
+
+int N, M;
+vector<edge> edges;
+long long dist[501];
+
+const long long INF = 1e10;
+
+int bellman_ford(){
+    dist[1] = 0;    
+    
+    // 벨만-포드 : O(VE)
+    for(int i=0; i<N-1; i++){
+        for(auto& edge : edges){
+            
+            // 시작 정점에서 도달할 수 없는 정점인 경우는 pass
+            // 도달할 수 없는 곳(비용:INF)을 거쳐서 가는 비용이
+            // 더 작은 값일 수는 없으므르..
+            if(dist[edge.src] == INF)
+                continue;
+            
+            // 간선(edge)를 거쳐서 dst에 도달하는 비용이
+            // 더 적은 경우 --> 갱신
+            if(dist[edge.dest] > dist[edge.src] + edge.cost){
+                dist[edge.dest] = dist[edge.src] + edge.cost;
+            }
+        }
+    }
+    
+    // 음수 가중치 사이클 검사
+    // 한 번 더 간선(edge) 검사
+        // 만약, 최단 거리 테이블(dist)이 더 작은 값으로 갱신되면
+        // 음수 가중치 사이클 (음의 무한) 발생
+    for(auto& edge : edges){
+        
+        // 시작 정점에서 도달할 수 없는 정점 --> pass
+        if(dist[edge.src] == INF)
+            continue;
+        if(dist[edge.dest] > dist[edge.src] + edge.cost){
+            return 0;
+        }
+    }
+    
+    return 1;
+}
+
+int main(){
+    cin >> N >> M;
+    
+    for(int i=0; i<M; i++){
+        int A, B, C;
+        cin >> A >> B >> C;
+        edges.push_back({A, B, C});
+    }
+
+    fill_n(dist, 501, INF);
+
+    int ret = bellman_ford();
+    
+    if(ret == 0)
+        cout << "-1";
+    else{
+        for(int i=2; i<=N; i++){
+            if(dist[i] == INF)
+                cout << "-1\n";
+            else 
+                cout << dist[i] << "\n";
+        }
+    }
+
+    return 0;
+}
