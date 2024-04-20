3967c7f9cdf8e17ec41a73aead1316ebb9628786
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-04-20 13:51:07 +0900

    Update A와 B.cc

--- 백준/Gold/12904. A와 B/A와 B.cc
+++ 백준/Gold/12904. A와 B/A와 B.cc
@@ -3,6 +3,17 @@
 #include <algorithm>
 using namespace std;
 
+// 틀린 이유
+// 방법 1)
+    // 재귀로 푸니까 시간초과 에러 발생
+// 방법 2)
+    // deque를 사용해서 flag로 앞, 뒤 관리
+    // 'B'가 나오면 flag 반전
+    // 근데, 찾지 못한 반례가 있었던 듯
+// 방법 3)
+    // algorithm 헤더 파일의 reverse() 함수 사용
+    // 라이브러리 함수 사용하는거라 시간 소요 많이 될 줄 알았는데 아니었다.~
+
 int main(){ 
     string s, t;
     cin >> s;
@@ -43,4 +54,3 @@ int main(){
 
     return 0;
 }
-
