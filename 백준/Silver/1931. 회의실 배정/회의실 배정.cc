451bdf02f4290bcf9e9db03b36f6ff7f19f520fc
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-06-23 22:14:33 +0900

    [Silver I] Title: 회의실 배정, Time: 88 ms, Memory: 3572 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -3,38 +3,30 @@
 #include <algorithm>
 using namespace std;
 
-
-bool comp(pair<int,int> o1, pair<int,int> o2){
-    if(o1.second != o2.second)
-        return o1.second < o2.second;
-    else 
-        return o1.first < o2.first;
-}
-
 int main(){
     int N;  cin >> N;
-    vector<pair<int,int>> v;
+    
+    vector<pair<int,int>> v;    
     
     for(int i=0; i<N; i++){
         int s, e;
         cin >> s >> e;
-        v.push_back({s,e});
+        v.push_back({s, e});
     }
-   
-    sort(v.begin(), v.end(), comp);
     
-    int limit = (*v.end()).second;
-    int end = v[0].second;
-    int cnt = 1;
+    sort(v.begin(), v.end());   
     
-    for(int i=1; i<N; i++){
-        if(end <= v[i].first){
-            cnt++;
-            end = v[i].second;
+    int ans = 1;
+    int prev = v[N-1].first;
+    
+    for(int i=N-2; i>=0; i--){
+        if(v[i].second <= prev){
+            prev = v[i].first;
+            ans++;
         }
     }
     
-    cout << cnt;
-    
+    cout << ans;
+
     return 0;
 }
