b05d14845f2f15ab7aa3c021b08abf9e0d646a4a
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-06-23 21:09:21 +0900

    [Silver III] Title: 피보나치 함수, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.cc
@@ -1,26 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int dp[41][2];
+int T;
+int fibo[41][2];
 
 int main(){
-    int T;  cin >> T;
+    cin >> T;
     
-    dp[0][0] = 1;
-    dp[0][1] = 0;
-    
-    dp[1][0] = 0;
-    dp[1][1] = 1;
+    fibo[0][0] = 1;    
+    fibo[1][1] = 1;
     
     for(int i=2; i<=40; i++){
-        dp[i][0] = dp[i-1][0] + dp[i-2][0];
-        dp[i][1] = dp[i-1][1] + dp[i-2][1];
+        fibo[i][0] = fibo[i-1][0] + fibo[i-2][0];
+        fibo[i][1] = fibo[i-1][1] + fibo[i-2][1];
     }
     
     while(T--){
         int N;  cin >> N;
-        cout << dp[N][0] << " " << dp[N][1] << "\n";
+        
+        cout << fibo[N][0] << " " << fibo[N][1] << "\n";
     }
     
+
     return 0;
 }
