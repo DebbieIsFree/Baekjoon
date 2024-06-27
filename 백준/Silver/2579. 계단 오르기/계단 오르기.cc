ec2259a90a53e9543a97ba745fe57d5d73c5dba4
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-06-27 16:36:43 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -2,25 +2,24 @@
 #include <algorithm>
 using namespace std;
 
-int dp[301][3];     // [][0] : dummy
 int arr[301];
+int dp[301][3];
 
 int main(){
-
     int N;  cin >> N;
-    
+
     for(int i=1; i<=N; i++){
-        cin >> arr[i];    
+        cin >> arr[i];
     }
     
     dp[1][1] = arr[1];
-    
+
     for(int i=2; i<=N; i++){
         dp[i][1] = max(dp[i-2][1], dp[i-2][2]) + arr[i];
         dp[i][2] = dp[i-1][1] + arr[i];
     }
-
-    cout << max(dp[N][1] , dp[N][2]);
+    
+    cout << max(dp[N][1], dp[N][2]);
 
     return 0;
 }
