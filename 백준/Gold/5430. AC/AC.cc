ff45ad0437f307694388d2e61ae3a92c8ed082a1
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-06-30 11:13:16 +0900

    [Gold V] Title: AC, Time: 52 ms, Memory: 11056 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.cc
+++ 백준/Gold/5430. AC/AC.cc
@@ -1,85 +1,75 @@
 #include <iostream>
-#include <deque>
 #include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-// 틀린 이유 : 
-// 문자열 파싱
-    // 예외) 한 자리 수 ~ 최대 세 자리 수 (100) 
-    // 예외) 숫자 입력 없이 빈 리스트 ([]) 일 때
-// deque
-// "error" 하고 개행해야 함!
-
-// 수행 함수 p 최대 길이         --> 10^5
-// 배열에 들어있는 수 최대 개수  --> 10^5
-// 매 수행 함수마다 배열 다 탐색하면 시간초과
-// 따라서, R연산 (배열 뒤집기)을 O(N)가 아닌 O(1)으로 풀어야 한다.
-
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
     int T;  cin >> T;
     
     while(T--){
-        string cmd; cin >> cmd;
-        int n;  cin >> n;
-        string input; cin >> input;
+        string cmd;      cin >> cmd;
+        int n;           cin >> n;
+        string nums;     cin >> nums;
         
-        deque<int> dq;
+        vector<string> v;
         
-        string str = "";
-        for(int i=0; i<input.length(); i++){
-            if(input[i] == ',' || input[i] == ']'){
-                if(str != "")
-                    dq.push_back(stoi(str));
-                str = "";
-            }else if(input[i] >= '0' && input[i] <= '9'){
-                str += input[i];
+        string tmp = "";
+        for(int i=1; i<nums.length(); i++){
+            if((nums[i] == ',' || nums[i] == ']') && tmp != ""){
+                v.push_back(tmp);
+                tmp = "";
+            }else{
+                tmp += nums[i];
             }
         }
         
-        bool flag = false;  // reverse가 아닌 상태 
-        bool output = false;    // "[ ]" 이렇게 출력할 수 있는 상태
+        bool flag = false;
+        int left = 0;
+        int right = v.size() - 1;
+        bool isReversed = false;
         
         for(int i=0; i<cmd.length(); i++){
             if(cmd[i] == 'R'){
-                flag = !flag;
-            }
-            else {
-                if(dq.empty()){
-                    cout << "error\n";
-                    output = true;
+                isReversed = !isReversed;
+            }else if(cmd[i] == 'D'){
+                if(left >= v.size() || right < 0 || v.size() == 0){
+                    flag = true;
                     break;
-                }else{
-                    if(!flag){
-                        dq.pop_front();
-                    }else{
-                        dq.pop_back();
-                    }
+                }else if(!isReversed){
+                    left = left + 1;
+                }else if(isReversed){
+                    right = right - 1;
                 }
             }
         }
-        if(output)
-            continue;
         
-
-        cout << "[";
-        if(!flag){
-            while(!dq.empty()){
-                cout << dq.front();
-                if(dq.size() > 1){
-                    cout << ",";
-                }
-                dq.pop_front();
-            }    
-        }else{
-            while(!dq.empty()){
-                cout << dq.back();
-                if(dq.size() > 1){
-                    cout << ",";
-                }
-                dq.pop_back();
-            }
+        if(flag){
+            cout << "error\n";
+            continue;
         }
-        cout << "]" << "\n";
+        
+        if(isReversed){
+            cout << "[";
+            for(int i=right; i>=left; i--){
+                cout << v[i];
+                if(i != left)
+                    cout << ",";
+            }
+            cout << "]\n";
+        }
+        else{
+            cout << "[";
+            for(int i=left; i<=right; i++){
+                cout << v[i];
+                if(i != right)
+                    cout << ",";
+            }
+            cout << "]\n";
+        }
     }
 
     return 0;
