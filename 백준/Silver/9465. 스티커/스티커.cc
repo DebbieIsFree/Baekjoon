695266d1031225b2e2896230207f397f8bfffccb
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-07-01 23:27:05 +0900

    [Silver I] Title: 스티커, Time: 76 ms, Memory: 4364 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.cc
+++ 백준/Silver/9465. 스티커/스티커.cc
@@ -2,32 +2,37 @@
 #include <algorithm>
 using namespace std;
 
-int arr[2][100001];
-int dp[2][100001];
+int arr[3][100001];
+int dp[3][100001];
 
 int main(){
-    int T; cin >> T;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    
+    int T;  cin >> T;
     
     while(T--){
         int n;  cin >> n;
         
-        for(int i=0; i<2; i++){
+        for(int i=1; i<=2; i++){
             for(int j=1; j<=n; j++){
                 cin >> arr[i][j];
             }
         }
         
-        dp[0][1] = arr[0][1];
         dp[1][1] = arr[1][1];
+        dp[2][1] = arr[2][1];
+        
+        // dp[1][2] = arr[2][1]+ arr[1][2];
+        // dp[2][2] = arr[1][1] + arr[2][2];
         
         for(int i=2; i<=n; i++){
-            dp[0][i] = max(dp[1][i-1] + arr[0][i], dp[1][i-2] + arr[0][i]);
-            dp[1][i] = max(dp[0][i-1] + arr[1][i], dp[0][i-2] + arr[1][i]);
+            dp[1][i] = max(dp[2][i-1], dp[2][i-2]) + arr[1][i];
+            dp[2][i] = max(dp[1][i-1], dp[1][i-2]) + arr[2][i];
         }
         
-        cout << max(dp[0][n], dp[1][n]) << "\n";
+        cout << max(dp[1][n], dp[2][n]) << "\n";
     }
-    
 
     return 0;
 }
