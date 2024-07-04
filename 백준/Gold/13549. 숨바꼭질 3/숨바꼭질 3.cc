966ea31a044c3b7cec611850b2e80aeabc4312e2
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-07-04 15:05:21 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2252 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -1,48 +1,40 @@
 #include <iostream>
-#include <algorithm>
-#include <deque>
-#define INF 1e9
-
+#include <queue>
 using namespace std;
 
 int N, K;
-int cost[100001];
-deque<int> dq;
-
-// 0-1 bfs
+bool visited[100001];
 
 int main(){
     cin >> N >> K;
     
-    fill_n(cost, 100001, INF);
-    cost[N] = 0;
-    dq.push_back(N);
+    queue<pair<int,int>> q;
+    q.push({N, 0});
+    visited[N] = true;
     
-    while(!dq.empty()){
-        int cur = dq.front();
-        dq.pop_front();
+    while(!q.empty()){
+        int cur = q.front().first;
+        int cnt = q.front().second;
+        q.pop();
         
         if(cur == K){
-            cout << cost[K];
+            cout << cnt;
             break;
         }
         
-        // 가중치가 0인 것 --> 큐의 앞
-        if(cur*2 <=100000 && cost[cur] < cost[cur*2]){
-            cost[cur*2] = cost[cur];
-            dq.push_front(cur*2);
+        if(cur*2 <= 100000 && !visited[cur*2]){
+            visited[cur*2] = true;
+            q.push({cur*2, cnt});
         }
-        
-        // 그 다음에 1인 것 --> 큐의 뒤
-        if(cur-1 >= 0 && cost[cur] + 1 < cost[cur-1]){
-            cost[cur-1] = cost[cur] + 1;
-            dq.push_back(cur-1);
+        if(cur-1 >= 0 && !visited[cur-1]){
+            visited[cur-1] = true;
+            q.push({cur-1, cnt+1});
         }
-        if(cur+1 <= 100000 && cost[cur] + 1 < cost[cur+1]){
-            cost[cur+1] = cost[cur] + 1;
-            dq.push_back(cur+1);
+        if(cur+1 <= 100000 && !visited[cur+1]){
+            visited[cur+1] = true;
+            q.push({cur+1, cnt+1});
         }
     }
-    
+
     return 0;
 }
