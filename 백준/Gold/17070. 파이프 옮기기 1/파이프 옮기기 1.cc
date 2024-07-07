e0cc66f3f1eb1745c3dc2e8a274a2bd6d5ad1f89
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-07-07 16:59:48 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -1,11 +1,12 @@
 #include <iostream>
 using namespace std;
 
+int N;
+int dp[17][17][3];
 int arr[17][17];
-int dp[17][17][3];      // [row][col] →, ↘, ↓
 
 int main(){
-    int N;  cin >> N;
+    cin >> N;
     
     for(int i=1; i<=N; i++){
         for(int j=1; j<=N; j++){
@@ -17,24 +18,20 @@ int main(){
     
     for(int i=1; i<=N; i++){
         for(int j=1; j<=N; j++){
-            if(i==1 && (j==1 || j==2))
+            if(i == 1 && j == 2)
                 continue;
             
             if(arr[i][j] == 1)
                 continue;
             
             dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][1];
-            dp[i][j][2] = dp[i-1][j][1] + dp[i-1][j][2];
-            
-            if(arr[i-1][j] != 1 && arr[i][j-1] != 1){
+            if(arr[i-1][j] != 1 && arr[i][j-1] != 1)
                 dp[i][j][1] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
-            }
+            dp[i][j][2] = dp[i-1][j][2] + dp[i-1][j][1];
         }
     }
     
-    cout << dp[N][N][0] + dp[N][N][1] + dp[N][N][2];  
-
+    cout << dp[N][N][0] + dp[N][N][1] + dp[N][N][2];
+    
     return 0;
 }
-
-
