b248c0997115f34a1c2930089d056defbdb2e653
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-09-09 20:04:58 +0900

    [Silver V] Title: 다리 놓기, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,27 +1,27 @@
 #include <iostream>
-
 using namespace std;
 
+int T, N, M;
+int dp[30][30];
+
 int main(){
+    cin >> T;
     
-    
-    int T;  cin >> T;
-    
-    while(T--){
-        int N, M;
-        cin >> N >> M;
-        
-        long long res = 1;
-        int d = 1;
-        
-        for(int i=M; i>=M-N+1; i--){
-            res *= i;   
-            res /= d;
-            d++;
-        }
-        
-        cout << res << "\n";
+    for(int i=1; i<=29; i++){
+        dp[i][0] = 1;
+        dp[i][i] = 1;
     }
     
+    for(int i=2; i<=29; i++){
+        for(int j=1; j<i; j++){
+            dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+        }
+    }
+    
+    while(T--){
+        cin >> N >> M;
+        cout << dp[M][N] << "\n";
+    }
+
     return 0;
 }
