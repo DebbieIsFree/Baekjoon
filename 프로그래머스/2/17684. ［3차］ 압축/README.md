395dee4ef35d702b0bac053b0ed7116344da5a4c
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-11-03 16:29:28 +0900

    [level 2] Title: [3차] 압축, Time: 21.17 ms, Memory: 72.6 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/README.md
+++ 프로그래머스/2/17684. ［3차］ 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] [3차] 압축 - 17684 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17684) 
 
 ### 성능 요약
 
-메모리: 78.4 MB, 시간: 10.41 ms
+메모리: 72.6 MB, 시간: 21.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 23일 14:09:59
+2024년 11월 03일 16:29:25
 
 ### 문제 설명
 
--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -3,51 +3,43 @@ import java.util.*;
 
 class Solution {
     
-    static HashMap<String, Integer> hm = new HashMap<>();
-    static Queue<Integer> q = new ArrayDeque<>();
-    
     public int[] solution(String msg) {
-        int size = 26;
-        
-        for(int i=0; i<26; i++){
-            hm.put(String.valueOf((char)('A' + i)), i+1);
-        }
-        
-        String str = "";
-        
-        for(int i=0; i<msg.length(); ){
-            StringBuilder tmp = new StringBuilder();
-
-            while(i < msg.length()){
-                tmp.append(msg.charAt(i));
-                
-                if(hm.containsKey(tmp.toString())){
-                    str = tmp.toString();
-                    i++;
-                }
-                else{
-                    q.add(hm.get(str));
-                    size++;
-                    hm.put(tmp.toString(), size);
-                    break;
-                }
-            }
-        }
-        
-        if(!str.equals("")){
-             q.add(hm.get(str));
-        }
-       
-        int sz = q.size();
-        int answer[] = new int[sz];
-        int idx = 0;
-        
-        while(!q.isEmpty()){
-            answer[idx] = q.peek();
-            q.remove();
-            idx++;
-        }
-        
-        return answer;
+        HashMap<String, Integer> map = new HashMap<>();
+	    
+	    for(int i=0; i<26; i++){
+	        map.put(String.valueOf((char)('A'+i)), i+1);
+	    }
+	    
+	    List<Integer> list = new ArrayList<>();
+	    StringBuilder sb = new StringBuilder();
+	    int idx = 0;
+	    int cnt = 26;
+	    
+	    while(true){
+	        if(idx == msg.length()){
+	            int n = map.get(sb.toString());
+	            list.add(n);
+	            break;
+	        }
+	        
+	        if( map.containsKey(sb.toString() + msg.charAt(idx)) ){
+	            sb.append(msg.charAt(idx));
+	        }else{
+	            int n = map.get(sb.toString());
+	            list.add(n);
+	            map.put(sb.toString() + msg.charAt(idx), ++cnt);
+	            sb = new StringBuilder();
+	            sb.append(msg.charAt(idx));
+	        }
+	        idx++;
+	    }
+	    
+	    
+	    int answer[] = new int[list.size()];
+	    for(int i=0; i<list.size(); i++){
+	        answer[i] = list.get(i);
+	    }
+	    
+	    return answer;
     }
 }
