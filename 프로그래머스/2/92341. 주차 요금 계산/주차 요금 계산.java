441a59076884a7aef127fa50289455279029cd27
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2024-11-03 19:19:35 +0900

    [level 2] Title: 주차 요금 계산, Time: 9.62 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,97 +1,104 @@
 import java.util.*;
 
-class Solution {
-    static HashMap<String, Car> hm = new HashMap<>();
-    static TreeMap<String, Integer> result = new TreeMap<>();
+public class Solution{
     
-    public int[] solution(int[] fees, String[] records) {
+    static HashMap<Integer, Node> list = new HashMap<>();
+    static TreeMap<Integer, Integer> timeList = new TreeMap<>();
+    
+	public int[] solution(int[] fees, String[] records) {
         
-        for(int i=0; i<records.length; i++){
-            String[] tmp = records[i].split(" ");
+	    int len = records.length;
+	    
+	    for(int i=0; i<len; i++){
+	        String[] parsedStr = records[i].split(" ");
+	        
+	        String cmd = parsedStr[2];      
+	        int carNum = Integer.parseInt(parsedStr[1]);
+	        String[] time = parsedStr[0].split(":");
+	        
+	        int hour = Integer.parseInt(time[0]);
+	        int min = Integer.parseInt(time[1]);
+	        
+	        if(cmd.equals("IN")){
+	            list.put(carNum, new Node(hour, min));
+	        }else{
+	            Node node = list.get(carNum);
+	            list.remove(carNum);
+	            
+	            int inHour = node.inHour;
+	            int inMin = node.inMin;
+	            
+	            accumulateTime(carNum, inHour, inMin, hour, min);
+	        }
+	    }
+	    
+	    for(int carNum : list.keySet()){
+	        Node node = list.get(carNum);
+	        // list.remove(carNum);
+	        
+	        int inHour = node.inHour;
+            int inMin = node.inMin;
             
-            String carNum = tmp[1];
-            String sign = tmp[2];
-            
-            String[] time = tmp[0].split(":");
-            int hour = Integer.valueOf(time[0]);
-            int min = Integer.valueOf(time[1]);
-            
-            if(sign.equals("IN")){
-                hm.put(carNum, new Car(carNum, hour, min));
-            }else{
-                Car inCar = hm.get(carNum);
-                hm.remove(carNum);    
-                int inHour = inCar.hour;
-                int inMin = inCar.min;
-                accumulate(carNum, inHour, inMin, hour, min);
-            }
-        }
+            accumulateTime(carNum, inHour, inMin, 23, 59);
+	    }
+	    
+	    int[] answer = new int[timeList.size()];
+	    int idx = 0;
+	    
+	    for(int carNum : timeList.keySet()){
+	        int fee = calc(carNum, fees);
+	        answer[idx++] = fee;
+	    }
         
-        for(String key : hm.keySet()){
-            Car c = hm.get(key);
-            String carNum = c.carNum;
-            // hm.remove(carNum);
-            
-            int inHour = c.hour;
-            int inMin = c.min;
-            accumulate(carNum, inHour, inMin, 23, 59);
-        }
-        
-        TreeMap<String, Integer> ans = new TreeMap<>();
-        
-        int[] answer = new int[result.size()];
-        int idx = 0;
-        
-        for(String key : result.keySet()){
-            int total = result.get(key);
-            
-            if(total <= fees[0]){
-                ans.put(key, fees[1]);
-            }else{
-                total -= fees[0];
-                int res = fees[1];
-                
-                res += total/fees[2]*fees[3];
-                total = total % fees[2];
-                if(total != 0){
-                    res += fees[3];
-                }
-                ans.put(key, res);
-            }
-            answer[idx] = ans.get(key);
-            idx++;
-        }
         return answer;
-    }
-    
-    public void accumulate(String carNum, int inHour, int inMin, int hour, int min){
-        int sum = 0;
-
-        if(inMin <= min){
-            sum += (min - inMin);
-            sum += (hour - inHour) * 60;
+	}
+	
+	public void accumulateTime(int carNum, int inHour, int inMin, int outHour, int outMin){
+	    int totalMin = 0;
+	    
+	    if(inHour == outHour){
+            totalMin += (outMin - inMin);
         }else{
-            sum += (60 - inMin) + min;
-            hour -= 1;
-            sum += (hour - inHour) * 60;
+            outHour -= 1;
+            totalMin += (60 - inMin) + outMin;
+            if(outHour - inHour > 0)
+                totalMin += (outHour - inHour) * 60;
         }
-        if(result.containsKey(carNum)){
-            int prevSum = result.get(carNum);
-            result.put(carNum, prevSum + sum);
-        }else{
-            result.put(carNum, sum);
-        }
-    }
-    
-    public class Car{
-        String carNum;
-        int hour;
-        int min;
-        
-        public Car(String carNum, int hour, int min){
-            this.carNum = carNum;
-            this.hour = hour;
-            this.min = min;
-        }
-    }
+	    
+	    if(timeList.containsKey(carNum)){
+	        int val = timeList.get(carNum);
+	        timeList.put(carNum, val + totalMin);
+	    }else{
+	        timeList.put(carNum, totalMin);   
+	    }
+	}
+	
+	public int calc(int carNum, int[] fees){
+	    int totalMin = timeList.get(carNum);
+	    
+	    if(totalMin <= fees[0])
+	        return fees[1];
+	   
+	    // 기본 시간 초과
+        int res = fees[1];
+	    totalMin -= fees[0];
+	    
+	    res += (totalMin / fees[2]) * fees[3];
+	    totalMin %= fees[2];
+	    
+	    if(totalMin != 0)
+	        res += fees[3];
+	   
+	    return res;
+	}
+	
+	public class Node{
+	    int inHour;
+	    int inMin;
+	    
+	    public Node(int h, int m){
+	        this.inHour = h;
+	        this.inMin = m;
+	    }
+	}
 }
