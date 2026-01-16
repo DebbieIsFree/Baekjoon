6b6f4d8c837a7e01f47a426bfa1883108118dc7f
장지원 <83392219+DebbieIsFree@users.noreply.github.com>
2026-01-16 19:24:25 +0900

    [level 2] Title: 전화번호 목록, Time: 90.21 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -0,0 +1,48 @@
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    bool answer = true;
+    vector<string> v;
+    set<string> s;
+    
+    
+    // 유효성 검사 시간 초과
+    // 전화번호 길이가 짧은 순으로 정렬
+//     sort(phone_book.begin(), phone_book.end());
+    
+//     for(int i=0; i<phone_book.size(); i++){
+//         string number = phone_book[i];
+        
+//         if(!s.empty()){
+//             for(string str : s){
+//                 if(number.find(str) == 0){
+//                     answer = false;
+//                     return answer;
+//                 }
+//             }
+//             s.insert(number);
+//         }else{
+//             s.insert(number);
+//         }
+//     }
+    
+    // 전화번호 길이가 짧은 순으로 정렬
+    sort(phone_book.begin(), phone_book.end());
+    
+    for(int i=0; i<phone_book.size()-1; i++){
+        string str1 = phone_book[i];
+        string str2 = phone_book[i+1];
+        
+        if(str2.find(str1) == 0){
+            answer = false;
+            return answer;
+        }
+    }
+    
+    return answer;
+}
